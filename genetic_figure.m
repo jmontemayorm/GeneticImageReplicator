fa0524bd876141006b3c3fb711e86009bd2474ce
jmontemayorm <j.montemayor.mm@gmail.com>
2019-03-09 17:59:56 -0600

    Plot called deleted script
    Added code

--- genetic_figure.m
+++ genetic_figure.m
@@ -1,9 +1,26 @@
 %% Genetic figure
-% Get the best
-drawingSpecimen = bestSpecimen;
+% Empty canvas
+canvas = blankCanvas;
 
-% Draw
-draw_specimen
+% Extract data
+polygons = bestSpecimen * multiplier;
+
+% Start indices at 1
+polygons(:,1:4) = polygons(:,1:4) + 1;
+
+% Top X start
+polygons(polygons(:,1) > maxX, 1) = maxX;
+% Top X end
+polygons(polygons(:,2) > maxX, 2) = maxX;
+% Top Y start
+polygons(polygons(:,3) > maxY, 3) = maxY;
+% Top Y end
+polygons(polygons(:,4) > maxY, 4) = maxY;
+
+% Loop through polygons
+for p = 1:numOfPolygons
+    canvas(polygons(p,3):polygons(p,4),polygons(p,1):polygons(p,2)) = canvas(polygons(p,3):polygons(p,4),polygons(p,1):polygons(p,2)) + uint8(polygons(p,5));
+end
 
 % Display
 subplot(1,2,2)
